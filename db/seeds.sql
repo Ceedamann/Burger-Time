c5e6bb35efb15d5ac8d37634240933b73176ab6d
Carl Robinson Jr <CarlRobinsonJr@gmail.com>
2019-10-13 10:00:20 -0400

    created database added table, added colums names and inserted queries

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Royale w/Cheese', true);
+INSERT INTO burgers (burger_name) VALUES ('Le Big Mac ');
+INSERT INTO burgers (burger_name) VALUES ('Rodeo burger');
